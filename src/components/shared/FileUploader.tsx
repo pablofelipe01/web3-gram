1419e03888b7197280081d15a81798c244f6db61
Pablo Acebedo <pablofelipe@me.com>
2023-11-09 11:01:22 -0500

    .gif

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -54,7 +54,7 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
           <h3 className="base-medium text-light-2 mb-2 mt-6">
             Drag photo here
           </h3>
-          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
+          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG, GIF</p>
 
           <Button type="button" className="shad-button_dark_4">
             Select from computer
